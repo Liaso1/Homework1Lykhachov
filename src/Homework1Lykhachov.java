b00ce9b0e6382fae7fd2ec8c3f0659b8d5be7831
Liaso1 <liasol97@gmail.com>
2023-08-06 22:14:49 -0400

    second message

--- src/Homework1Lykhachov.java
+++ src/Homework1Lykhachov.java
@@ -1,6 +1,6 @@
 public class Homework1Lykhachov {
     public static void main(String[] args) {
         System.out.println("Hello git");
-
+        System.out.println("Hello git from Kyrylo");
     }
 }
